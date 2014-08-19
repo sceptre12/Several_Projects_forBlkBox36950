9d32e45bee3ae440be35a04016ead804952193b8
Xavier <xthom001@gmail.com>
2014-08-19 05:24:08 -0400

    js needs to be fixed for reaching the second page and then adding and removing the overflow css property on the html

--- P_ID/js/main.js
+++ P_ID/js/main.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
 	$(document.body).fadeIn(1500);
 });
+
+// jquery Slide 
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 2500, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
+$(document).ready(function(){
+	// calculates the height of section 2
+
+	var aboveHeight = $('#sect2').height ;
+
+	// when scroll
+	$(window).scroll(function(){
+
+		// checks to see if the window has scrolled down to aboveHeight
+		if( $(window).scrollTop() + 1 > aboveHeight){
+			// $('html').removeClass('noScroll');
+			alert("reach")
+		}
+		
+	});
+});
