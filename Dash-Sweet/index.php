8c537dee4919eaaf07fdbd348f049eab7f1d028d
Xavier <xthom001@gmail.com>
2014-09-14 15:32:46 -0400

    changed up the file structure

---
+++ Dash-Sweet/index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>A-OfSweets</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/template6css.css">	
+</head>
+<body>
+	
+	<div class="title_top">
+		<div class="img_banner">
+			
+		</div>
+		<!-- <h1>A Dash Of Sweet</h1> -->
+	</div>
+	
+				<nav id="menu">
+			<ul>
+				<li><a href="#">My Story</a></li>
+				<li><a href="#">Menu</a></li>
+				<li id="view" class="active">
+					<b>A Dash Of Sweet</b>
+					<ul class="clear" >
+						<li><a href="template5.php">Home Layout 1</a></li>
+						<li><a href="template6.php">Home Layout 2</a></li>
+						<li><a href="template7.php">Home Layout 3</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Specialties</a></li>
+				<li id="actsect" >Contact</li>
+			</ul>
+		</nav>
+		<div class="page_cover ">
+			<div class="contactSect">
+				<span class="gyp glyphicon glyphicon-remove"></span>
+				<h1 class="animated fadeInDown">Hungry Already?</h1><br>
+				<h4 class="animated fadeInDown">Send us your request</h4>
+				<form name='contactform' class="form" action="navbars/contact.php" method='POST'>
+					<div class="text animated fadeInDown">
+						<textarea name="message" form="contactform">Test Message</textarea>
+					</div>
+					<div class="name animated fadeInDown">
+						<input type="text" name="name" id="name" placeholder="Name" />
+					</div>
+					<div class="email animated fadeInDown">
+						<input type="text" name="email" id="email" placeholder="Email" />
+					</div>
+					<div class="number animated fadeInDown">
+						<input type="text" name="number" id="number" placeholder="Number" />
+					</div>
+					<div class="submit animated fadeInDown">
+						<input type="submit" value="Submit" />
+					</div>
+				</form>
+			</div>	
+		</div>	
+		<div class="top_alignment">
+			<div class="cent_align">
+					<h1>Island Style</h1>
+				<div class="btn_align"><b><a href="#">Welcome</a></b></div>
+			</div>
+		</div>
+
+	<!-- <div class="g_contain"> -->
+		<div class="gallery">
+	      <ul>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">Birkenau</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">Lol</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">fweonfwefw</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">ewfwefef</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">rtshrthart</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">erthearh</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">rtshrthart</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">erthearh</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">Birkenau</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">Lol</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">fweonfwefw</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">ewfwefef</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">rtshrthart</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">erthearh</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">rtshrthart</h1></div>
+	            </div>
+	        </li>
+	        <li>
+	          <div class="a">            
+	                <img src="img/img40.jpg" alt="" />
+	                <div class="info"><h1 class="title">erthearh</h1></div>
+	            </div>
+	        </li>
+	        
+	        
+
+	      </ul>
+    </div><!-- .gallery -->
+	<!-- </div> -->
+		
+	
+	
+	
+
+	
+	
+	
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="js/gallery.js"></script>
+	
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	
+	<script type="text/javascript" src="js/template6js.js"></script>
+
+	<script src="js/respond.js"></script>
+	<!-- START SOUND CODE V4.1 HTML5 -->
+	<script  type="text/javascript">
+	<!--
+
+	// PLAYER VARIABLES
+
+	// var mp3snd = "music/Calyspo.mp3";
+	// var oggsnd = "music/Calyspo.ogg";
+
+	document.write('<audio autoplay="autoplay">');
+	document.write('<source src="'+mp3snd+'" type="audio/mpeg">');
+	document.write('<source src="'+oggsnd+'" type="audio/ogg">');
+	document.write('<!--[if lt IE 9]>');
+	document.write('<bgsound src="'+mp3snd+'" loop="1">');
+	document.write('<![endif]-->');
+	document.write('</audio>');
+
+	//-->
+	</script>
+	
+	<!-- END SOUND CODE V4.1 -->
+</body>
+</html>
