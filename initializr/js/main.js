180be6d5e6e11bd6dd74a2ac5e13c0c44585b496
Xavier <xthom001@gmail.com>
2014-10-10 23:25:40 -0400

    working on Mtek

---
+++ initializr/js/main.js
@@ -0,0 +1,8 @@
+
+// js for the pdu about me page for the toggle button.. needs fixing still a bit buggy 
+// $('span.expandSlider').click(function(){$('div #slideOne').delay(100).css('margin-left','220px'); $('a#toggler-slideOne').css('margin-left','220px')});
+// $('span.collapseSlider').click(function(){$('div #slideOne').css('margin-left','0px'); $('a#toggler-slideOne').css('margin-left','0px')});
+
+if($('li').has('p')){
+	
+}
